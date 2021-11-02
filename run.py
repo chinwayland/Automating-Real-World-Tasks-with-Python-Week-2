a2f91c6578d66f45392517d18f72758dadc325d5
Wayland Chin <waylandchin@gmail.com>
2021-11-02 18:00:26 +0000

    add data to dictionary

--- run.py
+++ run.py
@@ -2,7 +2,17 @@
 import os
 import requests
 
-text_files_location = '/data/feedback/'
+directory_text = '/data/feedback/'
+dictionary = {}
 
-for filename in os.listdir(text_files_location):
-	print(filename)
+for filename in os.listdir(directory_text):
+	with open(directory_text + filename, 'r') as text:
+		line = text.readline()
+		dictionary['title'] = line
+		line = text.readline()
+		dictionary['name'] = line
+		line = text.readline()
+		dictionary['date'] = line
+		line = text.readline()
+		dictionary['feedback'] = line
+print(dictionary)
